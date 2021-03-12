2dba9862589c399c11a1a0f48cb8b4e760288c94
YoussefSaghbini <43181070+YoussefSaghbini@users.noreply.github.com>
2021-03-11 19:03:49 -0500

    Added classpath for test.classes

--- src/test/java/com/bookstore/controller/book/BookControllerTest.java
+++ src/test/java/com/bookstore/controller/book/BookControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:test.properties")
 public class BookControllerTest {
 
     @Autowired
