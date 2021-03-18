3bed9b8c849bd3efb5669f144e2d86ce1b95d871
Dmytro Sytnik <dimaarman@gmail.com>
2021-03-19 00:50:40 +0200

    Fixed bug with hooking up a react App component

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bulma/css/bulma.min.css";
+
 import App from "./App";
 
 ReactDOM.render(
-        <App />,
+    <App />,
     document.getElementById("root")
 );
