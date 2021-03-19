eda92c75df3bf622f44091cda9a1e9251c328d0d
Dmytro Sytnik <dimaarman@gmail.com>
2021-03-19 15:39:12 +0200

    Updated layout of About Page

--- src/main/js/pages/About.js
+++ src/main/js/pages/About.js
@@ -1,12 +1,41 @@
 import React from "react";
+import {Grid, Padding, Paper} from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
 
-const About = () => (
-    <div>
-        <h1 className="title is-1">This is the About Page</h1>
-        <p>
-            Amazin Book Store
-        </p>
-    </div>
-);
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        margin: 'auto',
+    },
+    headLine: {
+        textAlign: "center"
+    }
+}));
+
+let About = () => {
+    const classes = useStyles();
+
+    return(
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs>
+                    <h1 className={classes.headLine}>This is the About Page</h1>
+                </Grid>
+            </Grid>
+            <Grid container spacing={3}>
+                <Grid item xs>
+                    <Paper className={classes.paper}>
+                        <p>
+                            Amazing Book Store
+                        </p>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
+    )
+};
 
 export default About;
