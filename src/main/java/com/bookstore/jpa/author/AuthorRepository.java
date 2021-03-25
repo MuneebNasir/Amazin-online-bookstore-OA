553cf1d3a26a1ad6b8988c5934b93a09825613cc
decarbonite <ahmedromih@cmail.carleton.ca>
2021-03-25 19:42:08 -0400

    added searching by name endpoints

--- src/main/java/com/bookstore/jpa/author/AuthorRepository.java
+++ src/main/java/com/bookstore/jpa/author/AuthorRepository.java
@@ -8,4 +8,7 @@ public interface AuthorRepository extends CrudRepository<Author, Long> {
     Author findById(long id);
     void deleteById(long id);
     List<Author> findAll();
+    List<Author> findAuthorsByFirstName(String firstName);
+    List<Author> findAuthorsByLastName(String lastName);
+    List<Author> findAuthorsByFirstNameAndLastName(String firstName, String lastName);
 }
