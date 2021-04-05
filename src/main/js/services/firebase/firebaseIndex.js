c1379093edbccc957723f69f9d0032c5b2ed372b
Dmytro Sytnik <dimaarman@gmail.com>
2021-04-06 01:16:01 +0300

    Update firebase configuration file to use environmental variables

--- src/main/js/services/firebase/firebaseIndex.js
+++ src/main/js/services/firebase/firebaseIndex.js
@@ -2,24 +2,14 @@ import firebase from 'firebase'
 import 'firebase/auth'
 import 'firebase/app'
 
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_API_KEY,
-//     authDomain: process.env.REACT_APP_AUTHDOMAIN,
-//     databaseURL: process.env.REACT_APP_BASEURL,
-//     projectId: process.env.REACT_APP_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_APP_ID
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyAhGmF3MAXmO3myqoWsZcxEnHuKVZlb6y0",
-    authDomain: "amazing-online-bookstore-oa.firebaseapp.com",
-    databaseURL: "https://amazing-online-bookstore-oa-default-rtdb.firebaseio.com",
-    projectId: "amazing-online-bookstore-oa",
-    storageBucket: "amazing-online-bookstore-oa.appspot.com",
-    messagingSenderId: "543742428874",
-    appId: "1:543742428874:web:24b0921b246b537faad633",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_BASEURL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
